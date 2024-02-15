969ef1db5a147fdc44c77086f23575f797df35cb
MADElinessss <jyseen@naver.com>
2024-02-15 17:17:05 +0900

    [Feat] Realm import, model 데이터베이스 테이블 생성

---
+++ ReminderAppProject/Model/RealmModel.swift
@@ -0,0 +1,29 @@
+//
+//  RealmModel.swift
+//  ReminderAppProject
+//
+//  Created by Madeline on 2/15/24.
+//
+
+import Foundation
+import RealmSwift
+
+class ReminderTable: Object {
+    
+    @Persisted(primaryKey: true) var id: ObjectId
+    
+    @Persisted var title: String
+    @Persisted var memo: String
+    @Persisted var deadline: Date
+    @Persisted var tag: String
+    @Persisted var priority: String
+    
+    convenience init(title: String, memo: String, deadline: Date, tag: String, priority: String) {
+        self.init()
+        self.title = title
+        self.memo = memo
+        self.deadline = deadline
+        self.tag = tag
+        self.priority = priority
+    }
+}
